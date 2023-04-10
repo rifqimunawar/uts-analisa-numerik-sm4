4ae6bc081f538e7949101565c8bd6c8a8573e9ad
Rifqi Munawar <102643244+rifqimunawar@users.noreply.github.com>
2023-04-10 09:11:59 +0700

    Add files via upload

---
+++ uts-analisa-numerik.m
@@ -0,0 +1,58 @@
+# Created by Octave 7.3.0, Mon Apr 10 09:08:32 2023 GMT <unknown@RIFQI>
+# name: X
+# type: scalar
+1
+
+
+# name: Y
+# type: scalar
+3
+
+
+# name: ans
+# type: scalar
+3
+
+
+# name: f
+# type: matrix
+# rows: 1
+# columns: 3
+ 0 0 88.790000000000006
+
+
+# name: f_t1
+# type: scalar
+356.16000000000003
+
+
+# name: f_t2
+# type: scalar
+89.790000000000006
+
+
+# name: g
+# type: scalar
+9.8100000000000005
+
+
+# name: t1
+# type: scalar
+1
+
+
+# name: t2
+# type: scalar
+3
+
+
+# name: x
+# type: scalar
+1
+
+
+# name: y
+# type: scalar
+3
+
+
